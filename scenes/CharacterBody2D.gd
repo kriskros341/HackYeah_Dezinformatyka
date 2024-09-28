5fc3f2aab17f578138c6609b0e89dc1e9d198172
H4wk <quasand89@gmail.com>
2024-09-28 13:28:36 +0200

    asdadsa

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Variables to handle jumping
-var jumps_left = 1  # Allow one initial jump and one double jump
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -15,17 +14,11 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		if is_on_floor():
-			# Reset jumps when on the floor
-			jumps_left = 1
-			velocity.y = JUMP_VELOCITY
-		elif jumps_left > 0:
-			# Allow double jump
-			jumps_left -= 1
-			velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
